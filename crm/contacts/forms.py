a283479a7e83f1b3b9b04b0e6049f805c6140667
efe <victorefe5@gmail.com>
2024-11-04 01:52:54 +0100

    front end updated. nav bar now mobile friendly

--- crm/contacts/forms.py
+++ crm/contacts/forms.py
@@ -1,16 +1,87 @@
 
+# from django import forms
+# from django.contrib.auth.models import User
+# from .models import ContactDetail
+
+# class ContactDetailCreationForm(forms.ModelForm):
+#     email = forms.EmailField(required=True)
+#     first_name = forms.CharField(max_length=30)
+#     last_name = forms.CharField(max_length=150)
+
+#     class Meta:
+#         model = ContactDetail
+#         fields = ['status', 'tags', 'assigned_staff']  # Fields from ContactDetail
+
+    
+
+#     def save(self, commit=True):
+#         email = self.cleaned_data['email']
+#         first_name = self.cleaned_data['first_name']
+#         last_name = self.cleaned_data['last_name']
+
+#         contact_detail = super().save(commit=False)
+
+#         # Create or update the associated user
+#         if not contact_detail.user_id:
+#             # Create a new user
+#             user = User(
+#                 username=email,
+#                 email=email,
+#                 first_name=first_name,
+#                 last_name=last_name
+#             )
+#             user.set_unusable_password()  # User needs to set a password later
+#             if commit:
+#                 user.save()
+#                 contact_detail.user = user  # Link the new user to contact detail
+#         else:
+#             # Update the existing user
+#             user = contact_detail.user
+#             user.email = email
+#             user.first_name = first_name
+#             user.last_name = last_name
+#             if commit:
+#                 user.save()
+
+#         if commit:
+#             contact_detail.save()  # Save the ContactDetail instance
+
+#         return contact_detail
+
+
 from django import forms
 from django.contrib.auth.models import User
 from .models import ContactDetail
 
 class ContactDetailCreationForm(forms.ModelForm):
-    email = forms.EmailField(required=True)
-    first_name = forms.CharField(max_length=30)
-    last_name = forms.CharField(max_length=150)
+    email = forms.EmailField(required=True, widget=forms.EmailInput(attrs={
+        'class': 'mt-1 block w-full px-3 py-2 border border-gray-300 rounded-md focus:outline-none focus:ring focus:ring-teal-300',
+        'placeholder': 'Enter email'
+    }))
+    first_name = forms.CharField(max_length=30, widget=forms.TextInput(attrs={
+        'class': 'mt-1 block w-full px-3 py-2 border border-gray-300 rounded-md focus:outline-none focus:ring focus:ring-teal-300',
+        'placeholder': 'Enter first name'
+    }))
+    last_name = forms.CharField(max_length=150, widget=forms.TextInput(attrs={
+        'class': 'mt-1 block w-full px-3 py-2 border border-gray-300 rounded-md focus:outline-none focus:ring focus:ring-teal-300',
+        'placeholder': 'Enter last name'
+    }))
 
     class Meta:
         model = ContactDetail
         fields = ['status', 'tags', 'assigned_staff']  # Fields from ContactDetail
+
+        widgets = {
+            'status': forms.Select(attrs={
+                'class': 'mt-1 block w-full px-3 py-2 border border-gray-300 rounded-md focus:outline-none focus:ring focus:ring-teal-300'
+            }),
+            'tags': forms.CheckboxSelectMultiple(attrs={
+                'class': 'mt-1'
+            }),
+            'assigned_staff': forms.Select(attrs={
+                'class': 'mt-1 block w-full px-3 py-2 border border-gray-300 rounded-md focus:outline-none focus:ring focus:ring-teal-300'
+            }),
+        }
 
     def save(self, commit=True):
         email = self.cleaned_data['email']
--- crm/users/forms.py
+++ crm/users/forms.py
@@ -1,53 +1,183 @@
+# from django import forms
+# from django.contrib.auth.models import User, Group, Permission
+# from django.contrib.auth.forms import UserCreationForm
+
+
+# class UserEditForm(forms.ModelForm):
+#     groups = forms.ModelMultipleChoiceField(
+#         queryset=Group.objects.all(),
+#         required=False,
+#         widget=forms.CheckboxSelectMultiple,
+#         label="Roles (Groups)"
+#     )
+#     class Meta:
+#         model = User
+#         fields = ['username', 'email', 'first_name', 'last_name', 'groups']
+
+
+# class RoleEditForm(forms.ModelForm):
+#     permissions = forms.ModelMultipleChoiceField(
+#         queryset=Permission.objects.all(),
+#         widget=forms.CheckboxSelectMultiple,
+#         required=False
+#     )
+
+#     class Meta:
+#         model = Group
+#         fields = ['name', 'permissions']
+#         labels = {
+#             'name': 'Role Name',
+#             'permissions': 'Assign Permissions'
+#         }
+
+# class RoleCreationForm(forms.ModelForm):
+#     permissions = forms.ModelMultipleChoiceField(
+#         queryset=Permission.objects.all(),
+#         widget=forms.CheckboxSelectMultiple,
+#         required=False
+#     )
+
+#     class Meta:
+#         model = Group
+#         fields = ['name', 'permissions']
+#         widgets = {}
+#         labels = {
+#             'name': 'Role Name',
+#             'permissions': 'Assign Permissions'
+#         }
+
+
+# # class RegisterForm(UserCreationForm):
+    
+# #     class Meta:
+# #         model = User
+# #         fields = ('username', 'first_name', 'last_name', 'email', 'password1', 'password2')
+
+# # forms.py
+# from django import forms
+# from django.contrib.auth.models import User
+
+# class RegisterForm(forms.ModelForm):
+#     class Meta:
+#         model = User
+#         fields = ['username', 'email', 'password']
+#         widgets = {
+#             'username': forms.TextInput(attrs={
+#                 'class': 'w-full py-2 px-4 border border-gray-300 rounded-lg bg-gray-50 focus:outline-none focus:ring-2 focus:ring-teal-300',
+#                 'placeholder': 'Enter your username'
+#             }),
+#             'email': forms.EmailInput(attrs={
+#                 'class': 'w-full py-2 px-4 border border-gray-300 rounded-lg bg-gray-50 focus:outline-none focus:ring-2 focus:ring-teal-300',
+#                 'placeholder': 'Enter your email'
+#             }),
+#             'password': forms.PasswordInput(attrs={
+#                 'class': 'w-full py-2 px-4 border border-gray-300 rounded-lg bg-gray-50 focus:outline-none focus:ring-2 focus:ring-teal-300',
+#                 'placeholder': 'Enter your password'
+#             }),
+#         }
+
+
 from django import forms
 from django.contrib.auth.models import User, Group, Permission
-from django.contrib.auth.forms import UserCreationForm
-
 
 class UserEditForm(forms.ModelForm):
     groups = forms.ModelMultipleChoiceField(
         queryset=Group.objects.all(),
         required=False,
-        widget=forms.CheckboxSelectMultiple,
+        widget=forms.CheckboxSelectMultiple(attrs={
+            'class': 'w-full p-2 border border-gray-300 rounded-lg bg-gray-50 focus:outline-none focus:ring- focus:ring-teal-300'
+        }),
         label="Roles (Groups)"
     )
+    
     class Meta:
         model = User
         fields = ['username', 'email', 'first_name', 'last_name', 'groups']
+        widgets = {
+            'username': forms.TextInput(attrs={
+                'class': 'w-full py-2 px-4 border border-gray-300 rounded-lg bg-gray-50 focus:outline-none focus:ring-2 focus:ring-teal-300',
+                'placeholder': 'Enter username'
+            }),
+            'email': forms.EmailInput(attrs={
+                'class': 'w-full py-2 px-4 border border-gray-300 rounded-lg bg-gray-50 focus:outline-none focus:ring-2 focus:ring-teal-300',
+                'placeholder': 'Enter email'
+            }),
+            'first_name': forms.TextInput(attrs={
+                'class': 'w-full py-2 px-4 border border-gray-300 rounded-lg bg-gray-50 focus:outline-none focus:ring-2 focus:ring-teal-300',
+                'placeholder': 'Enter first name'
+            }),
+            'last_name': forms.TextInput(attrs={
+                'class': 'w-full py-2 px-4 border border-gray-300 rounded-lg bg-gray-50 focus:outline-none focus:ring-2 focus:ring-teal-300',
+                'placeholder': 'Enter last name'
+            }),
+        }
 
 
 class RoleEditForm(forms.ModelForm):
     permissions = forms.ModelMultipleChoiceField(
         queryset=Permission.objects.all(),
-        widget=forms.CheckboxSelectMultiple,
-        required=False
+        widget=forms.CheckboxSelectMultiple(attrs={
+            'class': 'p-2 border border-gray-300 rounded-lg bg-gray-50 focus:outline-none focus:ring-2 focus:ring-teal-300'
+        }),
+        required=False,
+        label="Assign Permissions"
     )
-
+    
     class Meta:
         model = Group
         fields = ['name', 'permissions']
+        widgets = {
+            'name': forms.TextInput(attrs={
+                'class': 'w-full py-2 px-4 border border-gray-300 rounded-lg bg-gray-50 focus:outline-none focus:ring-2 focus:ring-teal-300',
+                'placeholder': 'Enter role name'
+            }),
+        }
         labels = {
             'name': 'Role Name',
             'permissions': 'Assign Permissions'
         }
+
 
 class RoleCreationForm(forms.ModelForm):
     permissions = forms.ModelMultipleChoiceField(
         queryset=Permission.objects.all(),
-        widget=forms.CheckboxSelectMultiple,
-        required=False
+        widget=forms.CheckboxSelectMultiple(attrs={
+            'class': 'p-2 border border-gray-300 rounded-lg bg-gray-50 focus:outline-none focus:ring-2 focus:ring-teal-300'
+        }),
+        required=False,
+        label="Assign Permissions"
     )
-
+    
     class Meta:
         model = Group
         fields = ['name', 'permissions']
+        widgets = {
+            'name': forms.TextInput(attrs={
+                'class': 'w-full py-2 px-4 border border-gray-300 rounded-lg bg-gray-50 focus:outline-none focus:ring-2 focus:ring-teal-300',
+                'placeholder': 'Enter role name'
+            }),
+        }
         labels = {
             'name': 'Role Name',
             'permissions': 'Assign Permissions'
         }
 
 
-class RegisterForm(UserCreationForm):
-    
+class RegisterForm(forms.ModelForm):
     class Meta:
         model = User
-        fields = ('username', 'first_name', 'last_name', 'email', 'password1', 'password2')
+        fields = ['username', 'email', 'password']
+        widgets = {
+            'username': forms.TextInput(attrs={
+                'class': 'w-full py-2 px-4 border border-gray-300 rounded-lg bg-gray-50 focus:outline-none focus:ring-2 focus:ring-teal-300',
+                'placeholder': 'Enter your username'
+            }),
+            'email': forms.EmailInput(attrs={
+                'class': 'w-full py-2 px-4 border border-gray-300 rounded-lg bg-gray-50 focus:outline-none focus:ring-2 focus:ring-teal-300',
+                'placeholder': 'Enter your email'
+            }),
+            'password': forms.PasswordInput(attrs={
+                'class': 'w-full py-2 px-4 border border-gray-300 rounded-lg bg-gray-50 focus:outline-none focus:ring-2 focus:ring-teal-300',
+                'placeholder': 'Enter your password'
+            }),
+        }
